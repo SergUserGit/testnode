dae0e6db86042e7ec1ad68473ce0d6ed6bf3ef93
Sergii Podriezienko <podreenko1986@gmail.com>
2024-08-26 12:20:53 +0300

    add: new rotes
    add: new rotes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import express from "express";
 
+import planets from "./planets.js";
+
 const app = express();
 
-app.get("/", routOne);
-app.get("/astrology", routTwo);
+app.get("/planets", getArrayPlanet);
+
+/*app.get("/", routOne);
+app.get("/astrology", routTwo);*/
 
 app.listen(3000, () => console.log("Server is runningn"));
 
-function routOne(request, response) {
+function getArrayPlanet(req, res) {
+  res.send(planets);
+}
+
+/*function routOne(request, response) {
   response.send("<h2>My page</h2>");
 }
 
@@ -15,4 +23,4 @@ function routTwo(request, response) {
   console.log(request.url);
   console.log(request.method);
   response.send("<h2>Astrology page</h2>");
-}
+}*/
---
+++ planets.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: "abc111",
+    planet: "Merkuriy",
+    degr: 5,
+    hours: 56,
+  },
+  {
+    id: "abc112",
+    planet: "Venera",
+    degr: 23,
+    hours: 52,
+  },
+];
