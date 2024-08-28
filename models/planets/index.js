c9bc2c003425dcde59e8655420393e4221c3f83f
Sergii Podriezienko <podreenko1986@gmail.com>
2024-08-28 11:02:10 +0300

    add: new function
    add: new function

--- models/planets/index.js
+++ models/planets/index.js
@@ -3,3 +3,8 @@ import planets from "../../planets.js";
 export const getAllPlanets = () => {
   return planets;
 };
+
+export const getAllPlanetsById = (id) => {
+  const result = planets.find((item) => item.id === id);
+  return result || null;
+};
--- routes/api/planets-router.js
+++ routes/api/planets-router.js
@@ -2,17 +2,37 @@ import express from "express";
 
 import * as planetServices from "../../models/planets/index.js";
 
+import { httpError } from "../../helpers/index.js";
+
 const planetRouter = express.Router();
 
-planetRouter.get("/", (req, res) => {
-  const result = planetServices.getAllPlanets();
-  res.json(result);
+planetRouter.get("/", async (req, res) => {
+  try {
+    const result = await planetServices.getAllPlanets();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
-/*planetRouter.get("/:id", (req, res) => {
-  res.json(planets[0]);
+planetRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await planetServices.getAllPlanetsById(id);
+    if (!result) {
+      throw httpError(404, `Planet with ${id} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
 });
 
+/*
 planetRouter.post("/", (req, res) => {
   res.json(planets[0]);
 });
@@ -25,8 +45,5 @@ planetRouter.delete("/:id", (req, res) => {
   res.json(planets[0]);
 });
 */
-function getPlanet() {
-  return planets;
-}
 
 export default planetRouter;
